6cd3d95cca1fbb6dc6209d0d56b16f50ebfeb8af
ChocoParrot <lachocoparrot@gmail.com>
2017-11-30 23:10:20 +0800

    Supposedly changed RBN

--- src/architectures/rbn.js
+++ src/architectures/rbn.js
@@ -3,7 +3,7 @@
 var Layer = require('../classes/Layer');
 var Linkage = require('../classes/Linkage');
 
-module.exports = function (layers, biasProbability, weightedBiases) {
+module.exports = function (layers, biasProbability) {
 
   if (layers.length < 2) {
     throw "[Neuras] Radial Basis Networks should have at least two layers!";
@@ -12,10 +12,10 @@ module.exports = function (layers, biasProbability, weightedBiases) {
   var layering = new Array();
 
   for (var i = 0; i < layers.length - 1; i++) {
-    layering.push(new Layer().addNeurones(layers[i], 'tanh').addBiases(biasProbability, weightedBiases));
+    layering.push(new Layer().addNeurones(layers[i], 'tanh').freezeNeurones().addBiases(biasProbability, false));
   };
 
-  layering.push(new Layer().addGates(layers[layers.length - 1], 'additive'));
+  layering.push(new Layer().addNeurones(layers[i], 'identity'));
 
   return new Linkage(layering, true);
 
