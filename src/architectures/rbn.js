d1d0eb7523d5eef8150971cde8dba1c9d3b3fe87
ChocoParrot <lachocoparrot@gmail.com>
2017-11-30 22:25:27 +0800

    RBN Bias support

--- src/architectures/rbn.js
+++ src/architectures/rbn.js
@@ -3,7 +3,7 @@
 var Layer = require('../classes/Layer');
 var Linkage = require('../classes/Linkage');
 
-module.exports = function (layers) {
+module.exports = function (layers, biasProbability) {
 
   if (layers.length < 2) {
     throw "[Neuras] Radial Basis Networks should have at least two layers!";
@@ -12,7 +12,7 @@ module.exports = function (layers) {
   var layering = new Array();
 
   for (var i = 0; i < layers.length - 1; i++) {
-    layering.push(new Layer().addNeurones(layers[i], 'sinc'));
+    layering.push(new Layer().addNeurones(layers[i], 'tanh').addBiases(biasProbability));
   };
 
   layering.push(new Layer().addGates(layers[layers.length - 1], 'additive'));
